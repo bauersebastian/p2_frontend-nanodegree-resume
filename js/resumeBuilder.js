3d003c36c7ed4f099728cac8f25f68b5ad11c5a7
Sebastian Bauer <sebastian@pisada.de>
2015-09-11 22:28:13 +0200

    buid the header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,8 @@
-$("#main").append("Sebastian Bauer");
+var fullname = "Sebastian Bauer";
+
+var formattedName = HTMLheaderName.replace("%data%", fullname);
+var formattedRole = HTMLheaderRole.replace("%data%", "SAP Consultant & Web Developer");
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+
